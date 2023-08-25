9205edbcad7573a19d614bcc73d1071683c139ef
Sandro Zappulla <sandro.zappulla@seculonia.net>
2023-08-25 16:52:14 +0200

    Zwischenstand 25.08.2023

--- try/main_try.py
+++ try/main_try.py
@@ -31,15 +31,23 @@ for dn, record in par.parse():
     #zeile+= dn + ","
     dn_split = dn.split(",")
 
+    # jeder dn's eintrag
     for dns in dn_split:
         #print(dns[:3])
+    # in dn's suche ou= (Die ersten 3 Zeichen werden überprüft)
         if dns[:3] == 'ou=':
+            # wenn ou= vorhanden, überprüfe ob es ein Subnets ist (ab der dritten Stelle wird überprüft)
             if dns[3:] == 'Subnets':
                 zeile+= "network"
                 zeile+= "," + search_string('cn', record)
                 net4 = ipaddress.ip_network(search_string('cn', record) + "/" + search_string('dhcpNetMask', record))
-                zeile += "," + str(net4.netmask) + "\n"
-                #zeile += search_string('cn', record)
+                zeile += "," + str(net4.netmask)
+
+                # routers
+                dhcp_option_split = search_string('dhcpOption', record).split(",")
+                for dhcp_option in dhcp_option_split:
+                    if dhcp_option[:7] == 'routers':
+                        zeile+= "," + dhcp_option[8:] + "\n"
 
     
     #zeile+= search_string_cut('owner', record, 3, 0) + ","
